ff7674fa7096f0556af4cc83dd3a37fa858715ab
jj-devel <jj.dev3l@gmail.com>
2025-09-10 00:05:47 -0400

    Added messages list for future message stream. Added Argv[1] input and Argv[2]{--verbose} for detailed stats. Made some formatting changes for a cleaner look & changed some variable names for clarity. :)

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import sys
+
+if len(sys.argv) < 1:
+    print("ERROR: Missing prompt conents, please add a <'string'> argument after the program call. ")
+    sys.exit(0)
+prompt = str(sys.argv[1])
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-first_question = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)])
+]
+
+output = client.models.generate_content(
+model="gemini-2.0-flash-001", 
+contents=messages, 
+)
+
+verbose_arg = f"""User prompt: {prompt}
+Prompt tokens: {output.usage_metadata.prompt_token_count}
+Response tokens: {output.usage_metadata.candidates_token_count}
+"""
 
 def main():
+    # Verbose
+    if "--verbose" in str(sys.argv):
+        print(verbose_arg)
     # Question
-    print(first_question.text)
-    # Token count
-    print(f"Prompt tokens: {first_question.usage_metadata.prompt_token_count}\nResponse tokens: {first_question.usage_metadata.candidates_token_count}")
+    print(f"Output: {output.text}")
 
 if __name__ == "__main__":
     main()
