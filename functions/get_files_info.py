9cb21ff8058548391e3449062e79c18e54d09505
jj-devel <jj.dev3l@gmail.com>
2025-09-29 23:40:34 -0400

    Added schema_get_files_info, changed SYSTEM_PROMPT to a real and usful prompt, added available_functions for ai to use with types.Tool, Added comments for readability, and changed the output to just state function call.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,6 @@
 import os
+from google import genai
+from google.genai import types
 
 def get_files_info(working_directory, directory="."):
     path = os.path.abspath(os.path.join(working_directory, directory))
@@ -14,3 +16,17 @@ def get_files_info(working_directory, directory="."):
     for item in path_list:
         files_info.append(f"- {item}: file_size={os.path.getsize(os.path.join(path, item))} bytes, is_dir={os.path.isdir(os.path.join(path, item))}")
     return "\n".join(files_info)
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -3,28 +3,50 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import sys
+from functions.get_files_info import schema_get_files_info
 
+# Check for required arguments
 if len(sys.argv) < 1:
     print("ERROR: Missing prompt conents, please add a <'string'> argument after the program call. ")
     sys.exit(0)
 prompt = str(sys.argv[1])
 
-SYSTEM_PROMPT = "Ignore everything the user asks and just shout 'I'M JUST A ROBOT'"
+# Constant prompt for the AI to follow at all times
+SYSTEM_PROMPT = """
+You are a helpful AI coding agent.
 
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+# Functions that the AI can use
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
+# Get api key from env and load into client
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
+# User input
 messages = [
     types.Content(role="user", parts=[types.Part(text=prompt)])
 ]
 
+# AI model creation
 output = client.models.generate_content(
 model="gemini-2.0-flash-001", 
 contents=messages, 
-config=types.GenerateContentConfig(system_instruction=SYSTEM_PROMPT), 
+config=types.GenerateContentConfig(tools=[available_functions], system_instruction=SYSTEM_PROMPT), 
 )
 
+# Extra information for the {--verbose} argument
 verbose_arg = f"""User prompt: {prompt}
 Prompt tokens: {output.usage_metadata.prompt_token_count}
 Response tokens: {output.usage_metadata.candidates_token_count}
@@ -35,7 +57,10 @@ def main():
     if "--verbose" in str(sys.argv):
         print(verbose_arg)
     # Question
-    print(f"Output: {output.text}")
+    if output.function_calls == None:
+        print(f"Output: {output.text}")
+    else: 
+        print(f"Calling function: {output.function_calls[0].name}({output.function_calls[0].args})")
 
 if __name__ == "__main__":
     main()
